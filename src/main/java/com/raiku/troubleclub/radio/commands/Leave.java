07e0b387732dbd08d3ed16031e49154cff3a8ca7
Raikuman <40256283+Raikuman@users.noreply.github.com>
2022-06-15 16:21:16 -0700

    Leave command uses method from stop command

--- src/main/java/com/raiku/troubleclub/radio/commands/Leave.java
+++ src/main/java/com/raiku/troubleclub/radio/commands/Leave.java
@@ -1,71 +1,28 @@
 package com.raiku.troubleclub.radio.commands;
 
-import com.raiku.troubleclub.radio.helpers.MessageResources;
 import com.raiku.troubleclub.radio.managers.command.CommandContext;
 import com.raiku.troubleclub.radio.managers.command.CommandInterface;
-import com.raiku.troubleclub.radio.music.GuildMusicManager;
-import com.raiku.troubleclub.radio.music.PlayerManager;
-import net.dv8tion.jda.api.entities.GuildVoiceState;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.TextChannel;
 
 import java.util.List;
 
 /**
  * Handles the bot leaving the voice channel of a user
  *
- * @version 1.0 2022-15-06
+ * @version 1.1 2022-15-06
  * @since 1.0
  */
 public class Leave implements CommandInterface {
 
 	@Override
 	public void invoke(CommandContext ctx) {
-		final TextChannel channel = ctx.getChannel();
-		final Member self = ctx.getGuild().getSelfMember();
-		final GuildVoiceState selfVoiceState = self.getVoiceState();
-		if (selfVoiceState == null) {
-			MessageResources.connectError(channel, 5);
-			return;
+		boolean stopped = new Stop().stopMusic(ctx);
+
+		if (stopped) {
+			ctx.getGuild().getAudioManager().closeAudioConnection();
+			ctx.getEvent().getMessage().addReaction("U+1F44B").queue();
+		} else {
+			ctx.getEvent().getMessage().addReaction("U+1F6AB").queue();
 		}
-
-		GuildVoiceState memberVoiceState = ctx.getEventMember().getVoiceState();
-		if (memberVoiceState == null) {
-			MessageResources.connectError(channel, 5);
-			return;
-		}
-
-		if (!memberVoiceState.inAudioChannel()) {
-			MessageResources.timedMessage(
-				"You must be in a voice channel to use this command",
-				channel,
-				5
-			);
-			return;
-		}
-
-		if (selfVoiceState.inAudioChannel() && (selfVoiceState.getChannel() != memberVoiceState.getChannel())) {
-			if (selfVoiceState.getChannel() == null) {
-				MessageResources.connectError(channel, 5);
-				return;
-			}
-
-			MessageResources.timedMessage(
-				"You must be in `" + selfVoiceState.getChannel().getName() + "` to use this command",
-				channel,
-				5
-			);
-			return;
-		}
-
-		ctx.getGuild().getAudioManager().closeAudioConnection();
-
-		// Clear current track and queue
-		final GuildMusicManager musicManager = PlayerManager.getInstance().getMusicManager(ctx.getGuild());
-		musicManager.trackScheduler.queue.clear();
-		musicManager.trackScheduler.audioPlayer.stopTrack();
-
-		ctx.getEvent().getMessage().addReaction("U+1F44B").queue();
 	}
 
 	@Override
