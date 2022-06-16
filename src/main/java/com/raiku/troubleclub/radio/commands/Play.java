af7c21aeb70b5f7084c41be9fe6ce90f59c1dfc9
Raikuman <40256283+Raikuman@users.noreply.github.com>
2022-06-15 20:23:12 -0700

    Play command resumes track if player is paused

--- src/main/java/com/raiku/troubleclub/radio/commands/Play.java
+++ src/main/java/com/raiku/troubleclub/radio/commands/Play.java
@@ -3,7 +3,9 @@ package com.raiku.troubleclub.radio.commands;
 import com.raiku.troubleclub.radio.helpers.MessageResources;
 import com.raiku.troubleclub.radio.managers.command.CommandContext;
 import com.raiku.troubleclub.radio.managers.command.CommandInterface;
+import com.raiku.troubleclub.radio.music.GuildMusicManager;
 import com.raiku.troubleclub.radio.music.PlayerManager;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import net.dv8tion.jda.api.entities.TextChannel;
 
 import java.net.URI;
@@ -29,11 +31,23 @@ public class Play implements CommandInterface {
 			ctx.getEvent().getMessage().addReaction("U+1F6AB").queue();
 
 		if (ctx.getArgs().isEmpty()) {
-			MessageResources.timedMessage(
-				"You must enter a valid link or search for a video",
-				ctx.getChannel(),
-				10
-			);
+			final GuildMusicManager musicManager = PlayerManager.getInstance().getMusicManager(ctx.getGuild());
+			AudioTrack currentTrack = musicManager.trackScheduler.audioPlayer.getPlayingTrack();
+
+			if (currentTrack == null) {
+				MessageResources.timedMessage(
+					"You must enter a valid link or search for a video",
+					ctx.getChannel(),
+					10
+				);
+				return;
+			}
+
+			if (musicManager.audioPlayer.isPaused()) {
+				musicManager.audioPlayer.setPaused(false);
+				ctx.getEvent().getMessage().addReaction("U+25B6").queue();
+				return;
+			}
 		}
 
 		String link = String.join(" ", ctx.getArgs());
