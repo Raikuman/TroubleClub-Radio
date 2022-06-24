0708049a937a4526be281957998b9ba87f891b3a
Raikuman <40256283+Raikuman@users.noreply.github.com>
2022-06-23 18:37:31 -0700

    Updated defaults for music config

--- src/main/java/com/raiku/troubleclub/radio/config/MusicConfig.java
+++ src/main/java/com/raiku/troubleclub/radio/config/MusicConfig.java
@@ -5,6 +5,12 @@ import com.raikuman.botutilities.configs.ConfigInterface;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ *  Provides configuration for music
+ *
+ * @version 1.0 2022-23-06
+ * @since 1.0
+ */
 public class MusicConfig implements ConfigInterface {
 
 	@Override
@@ -15,7 +21,7 @@ public class MusicConfig implements ConfigInterface {
 	@Override
 	public HashMap<String, String> getConfigs() {
 		return new HashMap<>(Map.ofEntries(
-			Map.entry("volume", "25")
+			Map.entry("volume", "100")
 		));
 	}
 }
