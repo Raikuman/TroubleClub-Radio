095a4a4e6be6d19fbf562f6416e237edfcc40269
Raikuman <40256283+Raikuman@users.noreply.github.com>
2024-02-22 00:22:58 -0800

    Add volume handling

--- src/main/java/com/raikuman/troubleclub/radio/database/MusicDatabaseHandler.java
+++ src/main/java/com/raikuman/troubleclub/radio/database/MusicDatabaseHandler.java
@@ -1,13 +1,16 @@
 package com.raikuman.troubleclub.radio.database;
 
+import com.raikuman.botutilities.config.ConfigData;
 import com.raikuman.botutilities.database.DatabaseManager;
 import com.raikuman.botutilities.defaults.database.DefaultDatabaseHandler;
+import com.raikuman.troubleclub.radio.config.MusicConfig;
 import net.dv8tion.jda.api.entities.Guild;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class MusicDatabaseHandler {
@@ -52,6 +55,63 @@ public class MusicDatabaseHandler {
             statement.execute();
         } catch (SQLException e) {
             logger.error("An error occurred deleting guild from music setting table for: " + guild.getName());
+        }
+    }
+
+    public static int getVolume(Guild guild, int trackNum) {
+        int guildId = DefaultDatabaseHandler.getGuildId(guild);
+
+        // Couldn't retrieve guild id
+        if (guildId == -1) {
+            return getDefaultVolume();
+        };
+
+        try (
+            Connection connection = DatabaseManager.getConnection();
+            PreparedStatement statement = connection.prepareStatement(
+                "SELECT track_" + trackNum + "_vol FROM guild_music_setting WHERE guild_id = ?"
+            )) {
+            statement.setInt(1, guildId);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return resultSet.getInt(1);
+                }
+            }
+
+            return getDefaultVolume();
+        } catch (SQLException e) {
+            logger.error("An error occurred getting volume for: " + guild.getName() + " with track: " + trackNum);
+            return getDefaultVolume();
+        }
+    }
+
+    public static void setVolume(Guild guild, int trackNum, int volume) {
+        int guildId = DefaultDatabaseHandler.getGuildId(guild);
+
+        // Couldn't retrieve guild id
+        if (guildId == -1) {
+            return;
+        };
+
+        try (
+            Connection connection = DatabaseManager.getConnection();
+            PreparedStatement statement = connection.prepareStatement(
+                "UPDATE guild_music_setting SET track_" + trackNum + "_vol = ? WHERE guild_id = ?"
+            )) {
+            statement.setInt(1, volume);
+            statement.setInt(2, guildId);
+            statement.execute();
+        } catch (SQLException e) {
+            logger.error("An error occurred setting volume for: " + guild.getName() + " with track: " + trackNum);
+        }
+    }
+
+    private static int getDefaultVolume() {
+        try {
+            return Integer.parseInt(new ConfigData(new MusicConfig()).getConfig("defaultvolume"));
+        } catch (NumberFormatException e) {
+            return 25;
         }
     }
 }
