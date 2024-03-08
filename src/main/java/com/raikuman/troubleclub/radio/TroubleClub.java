96566f623e3a79e335a9d815742dd7326307de14
Raikuman <40256283+Raikuman@users.noreply.github.com>
2024-03-07 17:25:22 -0800

    Use Bot-Utilities snapshot
    Update bot activity

--- src/main/java/com/raikuman/troubleclub/radio/TroubleClub.java
+++ src/main/java/com/raikuman/troubleclub/radio/TroubleClub.java
@@ -6,7 +6,9 @@ import com.raikuman.troubleclub.radio.config.music.MusicStartup;
 import com.raikuman.troubleclub.radio.config.PlaylistStartup;
 import com.raikuman.troubleclub.radio.database.music.MusicListener;
 import com.raikuman.troubleclub.radio.invoke.Invokes;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.ChunkingFilter;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
@@ -32,7 +34,7 @@ public class TroubleClub {
             .setAutoReconnect(true)
             .setRequestTimeoutRetry(true);
 
-        BotSetup
+        JDA jda = BotSetup
             .setup(jdaBuilder)
             .setConfigs(new MusicConfig())
             .setDatabases(new MusicStartup(), new PlaylistStartup())
@@ -40,5 +42,7 @@ public class TroubleClub {
             .addCommands(Invokes.getCommands())
             .addSlashes(Invokes.getSlashes())
             .build(System.getenv("RADIOTOKEN"));
+
+        jda.getPresence().setActivity(Activity.playing("music! \uD83C\uDFB9 | /radio"));
     }
 }
